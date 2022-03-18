7620ea4cfe604e0eb48776e0dba6dd841e37a3e2
aashishbansal23 <55803988+aashishbansal23@users.noreply.github.com>
2022-03-18 23:11:01 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (66.29%) - LeetHub

---
+++ 316-remove-duplicate-letters/316-remove-duplicate-letters.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string removeDuplicateLetters(string s) {
+        int len = s.length();
+        vector<int> map(26, 0);
+        for(int i=0; i<len; i++){
+            map[s[i]-'a']++;
+        }
+        
+        vector<bool> visited(26, false);
+        string ans = "";
+        for(int i=0; i<len; i++){
+            map[s[i]-'a']--;
+            if(!visited[s[i]-'a']){
+                while(ans.length()>0 && ans.back()>s[i] && map[ans.back()-'a']>0){
+                    visited[ans.back()-'a'] = false;
+                    ans.pop_back();
+                }
+                ans += s[i];
+                visited[s[i]-'a'] = true;
+            }
+        }
+        return ans;
+    }
+};
