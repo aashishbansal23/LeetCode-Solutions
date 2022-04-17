ae331584815587909b238df05206e9c3fd64d8e2
aashishbansal23 <55803988+aashishbansal23@users.noreply.github.com>
2022-04-17 08:51:06 +0530

    Time: 56 ms (42.61%), Space: 33.5 MB (19.37%) - LeetHub

--- 538-convert-bst-to-greater-tree/538-convert-bst-to-greater-tree.cpp
+++ 538-convert-bst-to-greater-tree/538-convert-bst-to-greater-tree.cpp
@@ -10,35 +10,35 @@
  * };
  */
 class Solution {
-//     void getAllElements(TreeNode* root, vector<int>& temp){
-//         if(root == NULL){
-//             return ;
-//         }
-//         getAllElements(root->left, temp);
-//         temp.push_back(root->val);
-//         getAllElements(root->right, temp);
-//     }
+    void getAllElements(TreeNode* root, vector<int>& temp){
+        if(root == NULL){
+            return ;
+        }
+        getAllElements(root->left, temp);
+        temp.push_back(root->val);
+        getAllElements(root->right, temp);
+    }
     
-//     TreeNode* createNewTree(TreeNode* root, vector<int>& temp){
-//         if(root == NULL){
-//             return NULL;
-//         }
+    TreeNode* createNewTree(TreeNode* root, vector<int>& temp){
+        if(root == NULL){
+            return NULL;
+        }
         
-//         int t = 0;
-//         for(int i=0; i<temp.size(); i++){
-//             if(root->val <= temp[i]){
-//                 t += temp[i];
-//             }
-//         }
-//         root->val = t;
-//         if(root->left != NULL){
-//             root->left = createNewTree(root->left, temp);
-//         }
-//         if(root->right != NULL){
-//             root->right = createNewTree(root->right, temp);
-//         }
-//         return root;
-//     }
+        int t = 0;
+        for(int i=0; i<temp.size(); i++){
+            if(root->val <= temp[i]){
+                t += temp[i];
+            }
+        }
+        root->val = t;
+        if(root->left != NULL){
+            root->left = createNewTree(root->left, temp);
+        }
+        if(root->right != NULL){
+            root->right = createNewTree(root->right, temp);
+        }
+        return root;
+    }
     
     int genereateNewTree(TreeNode* root, int adder){
         if(root == NULL){
@@ -56,11 +56,29 @@ class Solution {
         return root->val;
     }
     
+    void solve(TreeNode* root, int& sum){
+        if(root == NULL){
+            return ;
+        }
+        solve(root->right, sum);
+        sum += root->val;
+        root->val = sum;
+        solve(root->left, sum);
+    }
+    
 public:
     TreeNode* convertBST(TreeNode* root) {
-        int t = genereateNewTree(root, 0);
+        // Third Try
+        int sum = 0;
+        solve(root, sum);
         return root;
         
+        
+        // Second Try
+        // int t = genereateNewTree(root, 0);
+        // return root;
+        
+        // First Try
         // vector<int> temp;
         // getAllElements(root, temp);
         // return createNewTree(root, temp);
