46dca6a9ee26179310737e66faf63459cf28646f
aashishbansal23 <55803988+aashishbansal23@users.noreply.github.com>
2022-04-19 12:41:14 +0530

    Time: 27 ms (91.61%), Space: 57.8 MB (70.41%) - LeetHub

--- 99-recover-binary-search-tree/99-recover-binary-search-tree.cpp
+++ 99-recover-binary-search-tree/99-recover-binary-search-tree.cpp
@@ -28,12 +28,72 @@ class Solution {
         reCreateTree(root->right, store, i);
     }
     
+    void getLeftMax(TreeNode* root, TreeNode* & left_max_node, int& left_max_value){
+        if(root == NULL){
+            return ;
+        }
+        if(root->val > left_max_value){
+            left_max_value = root->val;
+            left_max_node = root;
+        }
+        getLeftMax(root->left, left_max_node, left_max_value);
+        getLeftMax(root->right, left_max_node, left_max_value);
+    }
+    
+    void getRightMin(TreeNode* root, TreeNode* & right_min_node, int& right_min_value){
+        if(root == NULL){
+            return ;
+        }
+        if(root->val < right_min_value){
+            right_min_value = root->val;
+            right_min_node = root;
+        }
+        getRightMin(root->left, right_min_node, right_min_value);
+        getRightMin(root->right, right_min_node, right_min_value);
+    }
+    
+    void reCreate(TreeNode* root){
+        if(root == NULL){
+            return ;
+        }
+        TreeNode* left_max_node = NULL;
+        TreeNode* right_min_node = NULL;
+        int left_max_value = INT_MIN;
+        int right_min_value = INT_MAX;
+        getLeftMax(root->left, left_max_node, left_max_value);
+        getRightMin(root->right, right_min_node, right_min_value);
+        if(left_max_node!=NULL && right_min_node!=NULL && left_max_node->val>root->val && right_min_node->val<root->val){
+            int temp = left_max_node->val;
+            left_max_node->val = right_min_node->val;
+            right_min_node->val = temp;
+        }else if(left_max_node!=NULL && left_max_node->val>root->val){
+            int temp = root->val;
+            root->val = left_max_node->val;
+            left_max_node->val = temp;
+        }else if(right_min_node!=NULL && right_min_node->val<root->val){
+            int temp = right_min_node->val;
+            right_min_node->val = root->val;
+            root->val = temp;
+        }
+        reCreate(root->left);
+        reCreate(root->right);
+    }
+    
 public:
     void recoverTree(TreeNode* root) {
-        vector<int> store;
-        getAllValues(root, store);
-        sort(store.begin(), store.end());
-        int i = 0;
-        reCreateTree(root, store, i);
+        // Second Try
+        if(root == NULL){
+            return ;
+        }
+        reCreate(root);
+        
+        
+        
+        // First Try
+        // vector<int> store;
+        // getAllValues(root, store);
+        // sort(store.begin(), store.end());
+        // int i = 0;
+        // reCreateTree(root, store, i);
     }
 };
