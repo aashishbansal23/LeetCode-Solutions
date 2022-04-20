d5252add7b3cb3dda66c5ecd5519ee4529608de7
aashishbansal23 <55803988+aashishbansal23@users.noreply.github.com>
2022-04-20 15:56:34 +0530

    Time: 0 ms (100.00%), Space: 6.9 MB (85.90%) - LeetHub

--- 289-game-of-life/289-game-of-life.cpp
+++ 289-game-of-life/289-game-of-life.cpp
@@ -1,72 +1,101 @@
 class Solution {
-    int getLiveNeigh(vector<vector<int>>& temp, int i, int j, int m, int n) {
-        int liveNeigh = 0;
-        if(i > 0) {
-            if(temp[i-1][j] == 1) {
-                liveNeigh++;
-            }
-        } 
-        
-        if(i < m-1) {
-            if(temp[i+1][j] == 1) {
-                liveNeigh++;
-            }
-        } 
-        
-        if(j > 0) {
-            if(temp[i][j-1] == 1) {
-                liveNeigh++;
+    bool check(vector<vector<int>>& temp, int i, int j, int m, int n){
+        int zero = 0;
+        int one = 0;
+        if(i>0 && j>0){
+            if(temp[i-1][j-1] == 0){
+                zero++;
+            }else{
+                one++;
             }
         }
         
-        if(j < n-1) {
-            if(temp[i][j+1] == 1) {
-                liveNeigh++;
-            }
-        } 
-        
-        if(i > 0 && j > 0) {
-            if(temp[i-1][j-1] == 1) {
-                liveNeigh++;
-            }
-        } 
-        
-        if(i < m-1 && j < n-1) {
-            if(temp[i+1][j+1] == 1) {
-                liveNeigh++;
-            }
-        } 
-        
-        if(i < m-1 && j > 0) {
-            if(temp[i+1][j-1] == 1) {
-                liveNeigh++;
-            }
-        } 
-        
-        if(i > 0 && j < n-1) {
-            if(temp[i-1][j+1] == 1) {
-                liveNeigh++;
+        if(i > 0){
+            if(temp[i-1][j] == 0){
+                zero++;
+            }else{
+                one++;
             }
         }
         
-        return liveNeigh;
+        if(i>0 && j<n){
+            if(temp[i-1][j+1] == 0){
+                zero++;
+            }else{
+                one++;
+            }
+        }
+        
+        if(j > 0){
+            if(temp[i][j-1] == 0){
+                zero++;
+            }else{
+                one++;
+            }
+        }
+        
+        if(j < n){
+            if(temp[i][j+1] == 0){
+                zero++;
+            }else{
+                one++;
+            }
+        }
+        
+        if(i<m && j>0){
+            if(temp[i+1][j-1] == 0){
+                zero++;
+            }else{
+                one++;
+            }
+        }
+        
+        if(i < m){
+            if(temp[i+1][j] == 0){
+                zero++;
+            }else{
+                one++;
+            }
+        }
+        
+        if(i<m && j<n){
+            if(temp[i+1][j+1] == 0){
+                zero++;
+            }else{
+                one++;
+            }
+        }
+        
+        if(temp[i][j] == 0){
+            if(one == 3){
+                return true;
+            }else{
+                return false;
+            }
+        }else{
+            if(one<2 || one>3){
+                return true;
+            }else{
+                return false;
+            }
+        }
     }
     
 public:
     void gameOfLife(vector<vector<int>>& board) {
         vector<vector<int>> temp = board;
         int m = board.size();
-	    int n = board[0].size();
-        for(int i=0; i < m; i++) {
-            for(int j=0; j < n; j++) {
-                int res = getLiveNeigh(temp, i, j, m, n);
-                if(board[i][j] == 0) {
-                    if(res == 3) {
-                        board[i][j] = 1;
-                    }
-                } else {
-                    if(res < 2 || res > 3) {
+        if(m <= 0){
+            return ;
+        }
+        int n = board[0].size();
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(check(temp, i, j, m-1, n-1)){
+                    if(board[i][j] == 1){
                         board[i][j] = 0;
+                    }else{
+                        board[i][j] = 1;
                     }
                 }
             }
