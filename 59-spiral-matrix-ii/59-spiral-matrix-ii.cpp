3879e05503fb0502f9a41f8d7faaa80db45cb77c
aashishbansal23 <55803988+aashishbansal23@users.noreply.github.com>
2022-04-20 16:09:56 +0530

    Time: 3 ms (33.12%), Space: 6.6 MB (19.20%) - LeetHub

--- 59-spiral-matrix-ii/59-spiral-matrix-ii.cpp
+++ 59-spiral-matrix-ii/59-spiral-matrix-ii.cpp
@@ -3,28 +3,26 @@ public:
     vector<vector<int>> generateMatrix(int n) {
         int r1 = 0, r2 = n-1;
         int c1 = 0, c2 = n-1;
-        int val = 0;
-        vector<vector<int>> v(n, vector<int>(n));
+        int curr = 1;
+        vector<vector<int>> ans(n, vector<int>(n));
         while(r1<=r2 && c1<=c2){
-            for(int i = c1; i <= c2; ++i){
-                v[r1][i] = ++val;
+            for(int i=c1; i<=c2; i++){
+                ans[r1][i] = curr++;
             }
-            for(int i = r1+1; i <= r2; ++i){
-                v[i][c2] = ++val;
+            for(int i=r1+1; i<=r2; i++){
+                ans[i][c2] = curr++;
             }
-            if(r1<r2 && c1<c2){
-                for(int i = c2-1; i>c1; --i){
-                    v[r2][i] = ++val;
-                }
-                for(int i = r2; i>r1; --i){
-                    v[i][c1] = ++val;
-                }
+            for(int i=c2-1; i>=c1; i--){
+                ans[r2][i] = curr++;
             }
-            ++r1;
-            --r2;
-            ++c1;
-            --c2;
+            for(int i=r2-1; i>r1; i--){
+                ans[i][c1] = curr++;
+            }
+            r1++;
+            r2--;
+            c1++;
+            c2--;
         }
-        return v;
+        return ans;
     }
 };
