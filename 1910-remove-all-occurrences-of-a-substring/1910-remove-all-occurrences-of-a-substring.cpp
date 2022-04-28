4aa2388cb458b228b4bf5971de79b5d08d50813e
aashishbansal23 <55803988+aashishbansal23@users.noreply.github.com>
2022-04-28 16:17:42 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (87.66%) - LeetHub

--- 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
+++ 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
@@ -1,44 +1,36 @@
 class Solution {
 public:
     string removeOccurrences(string s, string part) {
-        // int s_len = s.length();
-        // int p_len = part.length();
-        // int i = 0;
-        // int j = 0;
-        bool flag = true;
-        while(flag){
-            int s_len = s.length();
-            int p_len = part.length();
-            int i = 0;
-            int j = 0;
-            bool inner_flag = true;
-            for(j=0; j<=(s_len-p_len); j++){
-                if(inner_flag && s.substr(j, p_len)==part){
-                    j += p_len-1;
-                    inner_flag = false;
-                }else{
-                    s[i++] = s[j];
-                }
-            }
-            while(j < s_len){
-                s[i++] = s[j++];
-            }
-            s = s.substr(0, i);
-            if(i == j){
-                flag = false;
-            }
+        while(s.length()!=0 && s.find(part)<s.length()){
+            s.erase(s.find(part), part.length());
         }
         return s;
-        // for(j=0; j<=(s_len-p_len); j++){
-        //     if(s.substr(j, p_len) == part){
-        //         j += p_len-1;
-        //     }else{
-        //         s[i++] = s[j];
+        
+        
+        
+        // bool flag = true;
+        // while(flag){
+        //     int s_len = s.length();
+        //     int p_len = part.length();
+        //     int i = 0;
+        //     int j = 0;
+        //     bool inner_flag = true;
+        //     for(j=0; j<=(s_len-p_len); j++){
+        //         if(inner_flag && s.substr(j, p_len)==part){
+        //             j += p_len-1;
+        //             inner_flag = false;
+        //         }else{
+        //             s[i++] = s[j];
+        //         }
+        //     }
+        //     while(j < s_len){
+        //         s[i++] = s[j++];
+        //     }
+        //     s = s.substr(0, i);
+        //     if(i == j){
+        //         flag = false;
         //     }
         // }
-        // while(j < s_len){
-        //     s[i++] = s[j++];
-        // }
-        // return s.substr(0, i);
+        // return s;
     }
 };
