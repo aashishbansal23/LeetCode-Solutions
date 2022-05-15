1754d5d7607ab58c3888e33e95617123a69122e1
aashishbansal23 <55803988+aashishbansal23@users.noreply.github.com>
2022-05-15 10:49:33 +0530

    Time: 157 ms (26.34%), Space: 59.7 MB (76.50%) - LeetHub

--- 1302-deepest-leaves-sum/1302-deepest-leaves-sum.cpp
+++ 1302-deepest-leaves-sum/1302-deepest-leaves-sum.cpp
@@ -29,14 +29,45 @@ class Solution {
         getSum(root->right, height-1, ans);
     }
     
+    void helper(TreeNode* root, int& ans, int curr_height , int& max_height){
+        if(!root){
+            return ;
+        }
+        if(!root->left && !root->right){
+            if(max_height == -1){
+                ans = root->val;
+                max_height = curr_height;
+            }else if(max_height == curr_height){
+                ans += root->val;
+            }else if(curr_height > max_height){
+                ans = root->val;
+                max_height = curr_height;
+            }
+            return ;
+        }
+        helper(root->left, ans, curr_height+1, max_height);
+        helper(root->right, ans, curr_height+1, max_height);
+    }
+    
 public:
     int deepestLeavesSum(TreeNode* root) {
         if(root == NULL){
             return 0;
         }
-        int height = getHeight(root);
         int ans = 0;
-        getSum(root, height, ans);
+        int curr_height = 0;
+        int max_height = -1;
+        helper(root, ans, curr_height, max_height);
         return ans;
+        
+        
+        // First Try
+        // if(root == NULL){
+        //     return 0;
+        // }
+        // int height = getHeight(root);
+        // int ans = 0;
+        // getSum(root, height, ans);
+        // return ans;
     }
 };
