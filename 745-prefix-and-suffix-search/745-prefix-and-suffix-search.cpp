33af1968e314a8ca81e56dfbeff6101d97470386
aashishbansal23 <55803988+aashishbansal23@users.noreply.github.com>
2022-06-18 16:44:02 +0530

    Time: 1292 ms (7.84%), Space: 155.2 MB (58.21%) - LeetHub

---
+++ 745-prefix-and-suffix-search/745-prefix-and-suffix-search.cpp
@@ -0,0 +1,111 @@
+class WordFilter {
+    unordered_map<string, int> stringToIndex;
+    
+public:
+    WordFilter(vector<string>& words) {
+        int size = words.size();
+        for(int k=0; k<size; k++){
+            int len = words[k].length();
+            string word = words[k];
+            for(int i=len; i>0; i--){
+                for(int j=0; j<len; j++){
+                    string temp = word.substr(0,i)+"-"+word.substr(j, len-j);
+                    stringToIndex[temp] = k;
+                }
+            }
+        }
+    }
+    
+    int f(string prefix, string suffix) {
+        if(stringToIndex.count(prefix+"-"+suffix) > 0){
+            return stringToIndex[prefix+"-"+suffix];
+        }else{
+            return -1;
+        }
+    }
+};
+
+
+
+
+// First Try Gives TLE
+
+// class TrieNode{
+// public:
+//     char data;
+//     TrieNode* children[26] = {NULL};
+//     vector<int> indexes;
+//     TrieNode(char ch){
+//         data = ch;
+//     }
+// };
+
+// class Trie{
+//     TrieNode* root = new TrieNode('0');
+    
+// public:
+//     void insert(string& word, int& index){
+//         TrieNode* root = this->root;
+//         for(auto ch:word){
+//             root->indexes.push_back(index);
+//             // int i = ch-'a';
+//             if(root->children[ch-'a'] == NULL){
+//                 root->children[ch-'a'] = new TrieNode(ch);
+//             }
+//             root = root->children[ch-'a'];
+//         }
+//         root->indexes.push_back(index);
+//     }
+    
+//     vector<int> getStartsWith(string& word){
+//         TrieNode* root = this->root;
+//         for(auto ch:word){
+//             // int index = ch-'a';
+//             if(root->children[ch-'a'] == NULL){
+//                 return {};
+//             }else{
+//                 root = root->children[ch-'a'];
+//             }
+//         }
+//         return root->indexes;
+//     }
+// };
+
+// class WordFilter {
+//     Trie* dictPre = new Trie();
+//     Trie* dictSuff = new Trie();
+    
+// public:
+//     WordFilter(vector<string>& words) {
+//         int size = words.size();
+//         for(int i=0; i<size; i++){
+//             dictPre->insert(words[i], i);
+//             reverse(words[i].begin(),words[i].end());
+//             dictSuff->insert(words[i], i);
+//         }
+//     }
+    
+//     int f(string prefix, string suffix) {
+//         vector<int> preAns = dictPre->getStartsWith(prefix);
+//         reverse(suffix.begin(), suffix.end());
+//         vector<int> suffAns = dictSuff->getStartsWith(suffix);
+//         int i = preAns.size()-1;
+//         int j = suffAns.size()-1;
+//         while(i>=0 && j>=0){
+//             if(preAns[i] == suffAns[j]){
+//                 return preAns[i];
+//             }else if(preAns[i] > suffAns[j]){
+//                 i--;
+//             }else{
+//                 j--;
+//             }
+//         }
+//         return -1;
+//     }
+// };
+
+/**
+ * Your WordFilter object will be instantiated and called as such:
+ * WordFilter* obj = new WordFilter(words);
+ * int param_1 = obj->f(prefix,suffix);
+ */
