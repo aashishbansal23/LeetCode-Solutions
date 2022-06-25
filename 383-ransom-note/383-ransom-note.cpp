9b7d8762d1d5a150e80f96d158948900447bbc2d
aashishbansal23 <55803988+aashishbansal23@users.noreply.github.com>
2022-06-25 09:58:41 +0530

    Time: 33 ms (31.84%), Space: 8.8 MB (40.63%) - LeetHub

--- 383-ransom-note/383-ransom-note.cpp
+++ 383-ransom-note/383-ransom-note.cpp
@@ -1,23 +1,40 @@
 class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
-        sort(ransomNote.begin(), ransomNote.end());
-        sort(magazine.begin(), magazine.end());
-        int i=0, j=0;
-        while(i<ransomNote.length() && j<magazine.length()){
-            if(ransomNote[i] == magazine[j]){
-                i++;
-                j++;
-            }else if(ransomNote[i] > magazine[j]){
-                j++;
+        unordered_map<char, int> mp;
+        for(auto ch:magazine){
+            mp[ch]++;
+        }
+        for(auto ch:ransomNote){
+            if(mp[ch] > 0){
+                mp[ch]--;
             }else{
                 return false;
             }
         }
-        if(i == ransomNote.length()){
-            return true;
-        }else{
-            return false;
-        }
+        return true;
+        
+        
+        
+        
+        // First Try
+        // sort(ransomNote.begin(), ransomNote.end());
+        // sort(magazine.begin(), magazine.end());
+        // int i=0, j=0;
+        // while(i<ransomNote.length() && j<magazine.length()){
+        //     if(ransomNote[i] == magazine[j]){
+        //         i++;
+        //         j++;
+        //     }else if(ransomNote[i] > magazine[j]){
+        //         j++;
+        //     }else{
+        //         return false;
+        //     }
+        // }
+        // if(i == ransomNote.length()){
+        //     return true;
+        // }else{
+        //     return false;
+        // }
     }
 };
