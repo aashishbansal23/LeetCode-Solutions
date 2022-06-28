1116ebeb3291c592d4ce8c17986f0defc0c4562e
aashishbansal23 <55803988+aashishbansal23@users.noreply.github.com>
2022-06-29 00:30:43 +0530

    Time: 20 ms (61.54%), Space: 13.2 MB (53.10%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -3,25 +3,54 @@ public:
     void setZeroes(vector<vector<int>>& matrix) {
         int m = matrix.size();
         int n = matrix[0].size();
-        vector<int> cols;
+        int col0 = 1;
         for(int i=0; i<m; i++){
-            bool flag = false;
-            for(int j=0; j<n; j++){
+            if(matrix[i][0] == 0){
+                col0 = 0;
+            }
+            for(int j=1; j<n; j++){
                 if(matrix[i][j] == 0){
-                    flag = true;
-                    cols.push_back(j);
-                }
-            }
-            if(flag){
-                for(int k=0; k<n; k++){
-                    matrix[i][k] = 0;
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
                 }
             }
         }
-        for(auto j:cols){
-            for(int i=0; i<m; i++){
-                matrix[i][j] = 0;
+        for(int i=m-1; i>=0; i--){
+            for(int j=n-1; j>0; j--){
+                if(matrix[i][0]==0 || matrix[0][j]==0){
+                    matrix[i][j] = 0;
+                }
+            }
+            if(col0 == 0){
+                matrix[i][0] = 0;
             }
         }
+        
+        
+        
+        
+        // First Try space = O(n)
+        // int m = matrix.size();
+        // int n = matrix[0].size();
+        // vector<int> cols;
+        // for(int i=0; i<m; i++){
+        //     bool flag = false;
+        //     for(int j=0; j<n; j++){
+        //         if(matrix[i][j] == 0){
+        //             flag = true;
+        //             cols.push_back(j);
+        //         }
+        //     }
+        //     if(flag){
+        //         for(int k=0; k<n; k++){
+        //             matrix[i][k] = 0;
+        //         }
+        //     }
+        // }
+        // for(auto j:cols){
+        //     for(int i=0; i<m; i++){
+        //         matrix[i][j] = 0;
+        //     }
+        // }
     }
 };
