cef48bb53aad9dbe8cea28092bfc3b82a2ade92a
aashishbansal23 <55803988+aashishbansal23@users.noreply.github.com>
2022-07-19 23:15:43 +0530

    Time: 348 ms (38.20%), Space: 9 MB (66.10%) - LeetHub

--- 279-perfect-squares/279-perfect-squares.cpp
+++ 279-perfect-squares/279-perfect-squares.cpp
@@ -1,31 +1,44 @@
 class Solution {
 public:
     int numSquares(int n) {
-        set<int> s;
-        queue<int> q;
-        q.push(n);
-        s.insert(n);
-        int count = 1;
-        while(!q.empty()){
-            int size = q.size();
-            while(size-- > 0){
-                n = q.front();
-                q.pop();
-                int num = sqrt(n);
-                if(num*num == n){
-                    return count;
-                }
-                for(int i=1; i<n; i++){
-                    if(n-i*i>=0 && s.count(n-i*i)==0){
-                        q.push(n-i*i);
-                        s.insert(n-i*i);
-                    }else if(n-i*i < 0){
-                        break;
-                    }
-                }
+        vector<int> dp(n+1, INT_MAX);
+        dp[0] = 0;
+        for(int i=1; i<=n; i++){
+            for(int j=1; j*j<=i; j++){
+                dp[i] = min(dp[i], dp[i-j*j]+1);
             }
-            count++;
         }
-        return count;
+        return dp[n];
+        
+        
+        
+        
+        // First Try BFS
+        // set<int> s;
+        // queue<int> q;
+        // q.push(n);
+        // s.insert(n);
+        // int count = 1;
+        // while(!q.empty()){
+        //     int size = q.size();
+        //     while(size-- > 0){
+        //         n = q.front();
+        //         q.pop();
+        //         int num = sqrt(n);
+        //         if(num*num == n){
+        //             return count;
+        //         }
+        //         for(int i=1; i<n; i++){
+        //             if(n-i*i>=0 && s.count(n-i*i)==0){
+        //                 q.push(n-i*i);
+        //                 s.insert(n-i*i);
+        //             }else if(n-i*i < 0){
+        //                 break;
+        //             }
+        //         }
+        //     }
+        //     count++;
+        // }
+        // return count;
     }
 };
