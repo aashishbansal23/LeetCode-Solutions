05774c1ac541d9853f0840d7750e4116dd84c3c2
aashishbansal23 <55803988+aashishbansal23@users.noreply.github.com>
2022-07-19 20:50:53 +0530

    Time: 74 ms (77.03%), Space: 21.4 MB (37.82%) - LeetHub

---
+++ 1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int minCost(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        int dx[] = {-1, 1, 0, 0};
+        int dy[] = {0, 0, -1, 1};
+        int direction[] = {4, 3, 2, 1};
+        vector<vector<int>> dist(m, vector<int>(n, INT_MAX));
+        deque<pair<int,int>> q;
+        q.push_front({0,0});
+        dist[0][0] = 0;
+        while(!q.empty()){
+            auto [r,c] = q.front();
+            q.pop_front();
+            for(int i=0; i<4; i++){
+                int x = r+dx[i];
+                int y = c+dy[i];
+                if(x>=0 && x<m && y>=0 && y<n){
+                    int wt = 0;
+                    if(direction[i] != grid[r][c]){
+                        wt = 1;
+                    }
+                    if(dist[r][c]+wt < dist[x][y]){
+                        dist[x][y] = dist[r][c]+wt;
+                        if(wt == 1){
+                            q.push_back({x,y});
+                        }else{
+                            q.push_front({x,y});
+                        }
+                    }
+                }
+            }
+        }
+        return dist[m-1][n-1];
+    }
+};
