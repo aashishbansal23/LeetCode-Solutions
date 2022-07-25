de246558088eb03d2e8a3857adeac1da46aa310e
aashishbansal23 <55803988+aashishbansal23@users.noreply.github.com>
2022-07-25 15:49:57 +0530

    Time: 20 ms (58.01%), Space: 25.6 MB (60.58%) - LeetHub

--- 889-construct-binary-tree-from-preorder-and-postorder-traversal/889-construct-binary-tree-from-preorder-and-postorder-traversal.cpp
+++ 889-construct-binary-tree-from-preorder-and-postorder-traversal/889-construct-binary-tree-from-preorder-and-postorder-traversal.cpp
@@ -19,31 +19,51 @@ class Solution {
         return -1;
     }
     
+    TreeNode* constructFromPrePostHelp(vector<int>& preorder, vector<int>& postorder, int& preIndex, int& postIndex){
+        TreeNode* root = new TreeNode(preorder[preIndex++]);
+        if(root->val != postorder[postIndex]){
+            root->left = constructFromPrePostHelp(preorder, postorder, preIndex, postIndex);
+        }
+        if(root->val != postorder[postIndex]){
+            root->right = constructFromPrePostHelp(preorder, postorder, preIndex, postIndex);
+        }
+        postIndex++;
+        return root;
+    }
+    
 public:
     TreeNode* constructFromPrePost(vector<int>& preorder, vector<int>& postorder) {
-        unordered_set<int> visited;
-        queue<TreeNode*> q;
-        TreeNode* root = new TreeNode(preorder[0]);
-        q.push(root);
-        visited.insert(preorder[0]);
-        while(!q.empty()){
-            TreeNode* frontNode = q.front();
-            q.pop();
-            int index = getIndex(preorder, frontNode->val);
-            if(index+1<preorder.size() && visited.count(preorder[index+1])==0){
-                TreeNode* left = new TreeNode(preorder[index+1]);
-                frontNode->left = left;
-                visited.insert(preorder[index+1]);
-                q.push(left);
-            }
-            index = getIndex(postorder, frontNode->val);
-            if(index>0 && visited.count(postorder[index-1])==0){
-                TreeNode* right = new TreeNode(postorder[index-1]);
-                frontNode->right = right;
-                visited.insert(postorder[index-1]);
-                q.push(right);
-            }
-        }
-        return root;
+        int preIndex = 0;
+        int postIndex = 0;
+        return constructFromPrePostHelp(preorder, postorder, preIndex, postIndex);
+        
+        
+        
+        
+        // First Try
+        // unordered_set<int> visited;
+        // queue<TreeNode*> q;
+        // TreeNode* root = new TreeNode(preorder[0]);
+        // q.push(root);
+        // visited.insert(preorder[0]);
+        // while(!q.empty()){
+        //     TreeNode* frontNode = q.front();
+        //     q.pop();
+        //     int index = getIndex(preorder, frontNode->val);
+        //     if(index+1<preorder.size() && visited.count(preorder[index+1])==0){
+        //         TreeNode* left = new TreeNode(preorder[index+1]);
+        //         frontNode->left = left;
+        //         visited.insert(preorder[index+1]);
+        //         q.push(left);
+        //     }
+        //     index = getIndex(postorder, frontNode->val);
+        //     if(index>0 && visited.count(postorder[index-1])==0){
+        //         TreeNode* right = new TreeNode(postorder[index-1]);
+        //         frontNode->right = right;
+        //         visited.insert(postorder[index-1]);
+        //         q.push(right);
+        //     }
+        // }
+        // return root;
     }
 };
