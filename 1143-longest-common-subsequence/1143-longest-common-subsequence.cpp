314565f923c6e965aa0f262083301155e9d54bd1
aashishbansal23 <55803988+aashishbansal23@users.noreply.github.com>
2022-08-10 12:10:06 +0530

    Time: 35 ms (52.69%), Space: 13.2 MB (22.07%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -28,13 +28,26 @@ class Solution {
         return dp[index1][index2]=ans;
     }
     
+    int longestCommonSubsequenceDP(string& text1, string& text2){
+        vector<vector<int>> dp(text1.length()+1, vector<int>(text2.length()+1, 0));
+        for(int i=1; i<text1.length()+1; i++){
+            for(int j=1; j<text2.length()+1; j++){
+                dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+                if(text1[i-1] == text2[j-1]){
+                    dp[i][j] = max(dp[i][j], dp[i-1][j-1]+1);
+                }
+            }
+        }
+        return dp[text1.length()][text2.length()];
+    }
+    
 public:
     int longestCommonSubsequence(string text1, string text2) {
         // Recursion
         // return longestCommonSubsequenceRecursion(text1, text1.length(), text2, text2.length());
         
         // Memoization
-        vector<vector<int>> dp(text1.length()+1, vector<int>(text2.length()+1, -1));
+        // vector<vector<int>> dp(text1.length()+1, vector<int>(text2.length()+1, -1));
         // dp[0][0] = 0;
         // for(int i=1; i<text1.length()+1; i++){
         //     dp[i][0] = 0;
@@ -42,6 +55,9 @@ public:
         // for(int i=1; i<text2.length()+1; i++){
         //     dp[0][i] = 0;
         // }
-        return longestCommonSubsequenceMemoization(text1, text1.length(), text2, text2.length(), dp);
+        // return longestCommonSubsequenceMemoization(text1, text1.length(), text2, text2.length(), dp);
+        
+        // DP
+        return longestCommonSubsequenceDP(text1, text2);
     }
 };
