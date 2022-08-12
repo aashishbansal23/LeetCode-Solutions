b7287e46e1d1d60de5af77ea4e5551f371877b9b
aashishbansal23 <55803988+aashishbansal23@users.noreply.github.com>
2022-08-12 19:05:01 +0530

    Time: 26 ms (30.80%), Space: 6.1 MB (98.41%) - LeetHub

--- 10-regular-expression-matching/10-regular-expression-matching.cpp
+++ 10-regular-expression-matching/10-regular-expression-matching.cpp
@@ -28,9 +28,45 @@ class Solution {
         }
     }
     
+    bool isMatchMemoization(string& s, int i, string& p, int j, vector<vector<int>>& dp){
+        if(i==0 && j==0){
+            return true;
+        }
+        if(i>0 && j==0){
+            return false;
+        }
+        if(i==0 && j>0){
+            while(j > 0){
+                if(p[j-1] != '*'){
+                    return false;
+                }
+                j -= 2;
+            }
+            return true;
+        }
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+        if(s[i-1]==p[j-1] || p[j-1]=='.'){
+            return dp[i][j]=isMatchMemoization(s, i-1, p, j-1, dp);
+        }else if(p[j-1] == '*'){
+            if(s[i-1]==p[j-2] || p[j-2]=='.'){
+                return dp[i][j]=isMatchMemoization(s, i-1, p, j, dp) || isMatchMemoization(s, i-1, p, j-2, dp) || isMatchMemoization(s, i, p, j-2, dp);
+            }else{
+                return dp[i][j]=isMatchMemoization(s, i, p, j-2, dp);
+            }
+        }else{
+            return dp[i][j]=false;
+        }
+    }
+    
 public:
     bool isMatch(string s, string p) {
         // Recursion
         return isMatchRecursion(s, s.length(), p, p.length());
+        
+        // Memoization
+        vector<vector<int>> dp(s.length()+1, vector<int>(p.length()+1, -1));
+        return isMatchMemoization(s, s.length(), p, p.length(), dp);
     }
 };
