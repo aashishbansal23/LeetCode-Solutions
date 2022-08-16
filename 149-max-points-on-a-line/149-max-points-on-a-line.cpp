3040682121ec14e4da41bff1c570cbf3621014d9
aashishbansal23 <55803988+aashishbansal23@users.noreply.github.com>
2022-08-16 15:56:06 +0530

    Time: 39 ms (81.49%), Space: 13.4 MB (46.93%) - LeetHub

--- 149-max-points-on-a-line/149-max-points-on-a-line.cpp
+++ 149-max-points-on-a-line/149-max-points-on-a-line.cpp
@@ -6,10 +6,10 @@ public:
         for(int i=0; i<size-1; i++){
             unordered_map<double, int> mp;
             for(int j=i+1; j<size; j++){
-                if(points[i][1] == points[j][1]){
+                if(points[i][0] == points[j][0]){
                     mp[-5000]++;
                 }else{
-                    double slope = (double)(points[j][0]-points[i][0])/(double)(points[j][1]-points[i][1]);
+                    double slope = (double)(points[j][1]-points[i][1])/(double)(points[j][0]-points[i][0]);
                     mp[slope]++;
                 }
             }
