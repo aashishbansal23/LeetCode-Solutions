ec97cc93bd90749d8a1e03439a369a8f28a63203
aashishbansal23 <55803988+aashishbansal23@users.noreply.github.com>
2022-08-22 18:45:12 +0530

    Time: 8 ms (82.23%), Space: 9 MB (45.72%) - LeetHub

--- 684-redundant-connection/684-redundant-connection.cpp
+++ 684-redundant-connection/684-redundant-connection.cpp
@@ -14,29 +14,68 @@ class Solution {
         return false;
     }
     
+    int find(int i, vector<int>& parent){
+        if(parent[i] == -1){
+            return i;
+        }
+        return parent[i]=find(parent[i], parent);
+    }
+    
+    void unionSet(int i, int j, vector<int>& parent, vector<int>& rank){
+        int p1 = find(i, parent);
+        int p2 = find(j, parent);
+        if(p1 != p2){
+            if(rank[p1] > rank[p2]){
+                parent[p2] = p1;
+                rank[p1] += rank[p2];
+            }else{
+                parent[p1] = p2;
+                rank[p2] += rank[p1];
+            }
+        }
+    }
+    
 public:
     vector<int> findRedundantConnection(vector<vector<int>>& edges) {
         int n = edges.size();
-        vector<vector<int>> adj(n+1);
-        vector<bool> visited(n+1, false);
+        vector<int> parent(n+1, -1);
+        vector<int> rank(n+1, 1);
         for(auto edge:edges){
-            if(visited[edge[0]] && visited[edge[1]]){
-                vector<bool> tempVisited(n+1, false);
-                if(dfs(adj, edge[0], edge[1], tempVisited)){
-                    return edge;
-                }else{
-                    visited[edge[0]] = true;
-                    visited[edge[1]] = true;
-                    adj[edge[0]].push_back(edge[1]);
-                    adj[edge[1]].push_back(edge[0]);
-                }
+            int p1 = find(edge[0], parent);
+            int p2 = find(edge[1], parent);
+            if(p1 == p2){
+                return edge;
             }else{
-                adj[edge[0]].push_back(edge[1]);
-                adj[edge[1]].push_back(edge[0]);
-                visited[edge[0]] = true;
-                visited[edge[1]] = true;
+                unionSet(edge[0], edge[1], parent, rank);
             }
         }
         return {};
+        
+        
+        
+        
+        // First Try
+        // int n = edges.size();
+        // vector<vector<int>> adj(n+1);
+        // vector<bool> visited(n+1, false);
+        // for(auto edge:edges){
+        //     if(visited[edge[0]] && visited[edge[1]]){
+        //         vector<bool> tempVisited(n+1, false);
+        //         if(dfs(adj, edge[0], edge[1], tempVisited)){
+        //             return edge;
+        //         }else{
+        //             visited[edge[0]] = true;
+        //             visited[edge[1]] = true;
+        //             adj[edge[0]].push_back(edge[1]);
+        //             adj[edge[1]].push_back(edge[0]);
+        //         }
+        //     }else{
+        //         adj[edge[0]].push_back(edge[1]);
+        //         adj[edge[1]].push_back(edge[0]);
+        //         visited[edge[0]] = true;
+        //         visited[edge[1]] = true;
+        //     }
+        // }
+        // return {};
     }
 };
